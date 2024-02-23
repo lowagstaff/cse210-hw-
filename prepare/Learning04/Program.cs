dd61de7e45038dc3608bfc33149bcb9399b757ac
lowagstaff <wagstaffl97@gmail.com>
2024-02-23 12:07:03 -0700

    Finished Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new Assignment("Samuel Bennet", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+        MathAssignment m = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m.GetSummary());
+        Console.WriteLine(m.GetHomeworkList());
+        WritingAssignment w = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(w.GetSummary());
+        Console.WriteLine(w.GetWritingInformation());
     }
 }
